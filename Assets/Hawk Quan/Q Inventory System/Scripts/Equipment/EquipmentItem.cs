fb41210d5fa9221d7e39ecac4ee0418d62dbb654
veera <veera.suomine@gmail.com>
2022-05-08 14:51:04 +0300

    fixed errors, fixed pausemenu, added end game scene

--- Assets/Hawk Quan/Q Inventory System/Scripts/Equipment/EquipmentItem.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EquipmentItem : MonoBehaviour {
-    public Vector3 equipmentPosition;
-    public Vector3 equipmentRotation;
-}
---
+++ Assets/Scripts/Doors/TheEnd.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TheEnd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("TheEnd");
+            
+        }
+
+    }
+}
+
